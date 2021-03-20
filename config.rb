9f51a13eb575a0e48c63dfd2e67f112ff69e182c
leavendubey <l.dubey@dubey-peinture.ch>
2021-03-20 18:05:09 +0100

    adding middleman deploy method

--- config.rb
+++ config.rb
@@ -50,12 +50,12 @@ activate :google_analytics do |ga|
 ga.tracking_id = 'UA-'
 end
 
-# Middleman-deploy / Decommenter et remplacer ID et REPO par votre ID et repo gihub
-# activate :deploy do |deploy|
-#   deploy.build_before = true
-#   deploy.remote = 'https://github.com/ID/REPO.git'
-#   deploy.deploy_method = :git
-# end
+Middleman-deploy / Decommenter et remplacer ID et REPO par votre ID et repo gihub
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.remote = 'https://github.com/leavendubey/dubey-website.git'
+  deploy.deploy_method = :git
+end
 
 # root in development
 configure :build do
